d544d5537e4f58cd307f77c3e535c0b82f3767f9
Sonny <sis_0101@coresight.io>
2021-12-08 20:10:13 +0900

    추가

--- src/pages/QuoteDetail.tsx
+++ src/pages/QuoteDetail.tsx
@@ -1,2 +1,14 @@
 import React, { Fragment } from "react";
-import { Route, Link } from "react-router-dom";
+import { Route, Link, useMatch, useParams} from "react-router-dom";
+import axios from "axios";
+
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import LoadingSpinner from "../components/ui/LoadingSpinner";
+import Comments from "../components/comments/Comments";
+
+const QuoteDetail: React.FC = () => {
+  
+  return ();
+};
+
+export default QuoteDetail;
