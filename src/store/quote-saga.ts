427276beccf539ce4f7eb4376aa91b9275d9a8df
Sonny <libertines11@gmail.com>
2021-12-13 08:25:36 +0900

    '추가'

--- src/store/quote-saga.ts
+++ src/store/quote-saga.ts
@@ -2,7 +2,23 @@
 // https://mytutorials.tistory.com/421
 // https://redux-saga.js.org/docs/advanced/RootSaga
 
-import { all, fork, put, call } from "redux-saga/effects";
+import { all, fork, put, call, takeLatest } from "redux-saga/effects";
 import { getcomments, getCommentsReq, getCommentsError } from "./quotes-slice";
 import axios, { AxiosError, AxiosResponse } from "axios";
 import { FIREBASE_DOMAIN } from "./index";
+
+// FIREBASE_DOMAIN  로 검색해서 관련 API를 여기에 추가하고 사가도 처리하기!!
+
+function getAllCommentsAPI(data) {
+  return axios.post(``);
+}
+
+function* getAllComments() {}
+
+function* watchGetCommentReq() {
+  yield takeLatest();
+}
+
+export default function* quoteSaga() {
+  yield all([]);
+}
